d22b1cf6c84f4953a22b202efc401ed606ae6f0d
Quiquiles <129068122+Quiquiles@users.noreply.github.com>
2023-09-28 22:28:35 -0300

    Update Salario.c

--- Salario.c
+++ Salario.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-void salario_a(){
+void salario_a() {
   float valor1, resultado;
-  scanf("%f", &valor1); //armazena o valor
+  scanf("%f", &valor1);
   resultado = valor1 + (valor1 * 0.05);
   printf("R$ %.2f\n", resultado);
 }
 
-void salario_b(){ 
-    float valor1, resultado; 
-    scanf ("%f", &valor1); 
-    resultado = valor1 + (valor1 * 0.07); 
-    printf("R$ %.2f\n", resultado); 
+void salario_b() {
+  float valor1, resultado;
+  scanf("%f", &valor1);
+  resultado = valor1 + (valor1 * 0.07);
+  printf("R$ %.2f\n", resultado);
 }
 
-void salario_c(){
+void salario_c() {
   float valor1, resultado;
   scanf("%f", &valor1);
   resultado = valor1 + (valor1 * 0.08);
-  printf("R$ %.2f\n", resultado); //realizei quebra de linha
+  printf("R$ %.2f\n", resultado);
 }
 
 int main(void) {
   char entrada[15];
   scanf("%s", entrada);
 
-    if (strcmp(entrada, "a") == 0) {
-        salario_a();
-    } else if (strcmp(entrada, "b") == 0) {
-        salario_b();
-    }if (strcmp(entrada, "c") == 0) {
-        salario_c();
-    } 
-    
+  if (strcmp(entrada, "a") == 0) {
+    salario_a();
+  } else if (strcmp(entrada, "b") == 0) {
+    salario_b();
+  } if (strcmp(entrada, "c") == 0) {
+    salario_c();
+  }
+
   return 0;
-  
-} 
+}
