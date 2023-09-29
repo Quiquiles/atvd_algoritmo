646839ff387e5f2a6c7cdfabdea52fc5a71a189d
Quiquiles <129068122+Quiquiles@users.noreply.github.com>
2023-09-28 22:22:38 -0300

    Update Numeros.c

--- Numeros.c
+++ Numeros.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int numero1, numero2;
     int primeiroNumeroLido = 0;
 
     scanf("%d", &numero1);
     primeiroNumeroLido = numero1;
-    
+
     scanf("%d", &numero2);
 
     if (numero1 == numero2 && primeiroNumeroLido == numero2) {
@@ -14,16 +15,16 @@ int main() {
     } else if (numero1 > numero2) {
         printf("%d eh maior que %d\n", numero1, numero2);
     } else {
-    	printf("%d eh maior que %d\n", numero2, numero1);
-	} 
-	
-    if (numero1 != numero2) { // Verifica se os números são diferentes
-    if (numero1 % numero2 == 0 || numero2 % numero1 == 0) {
-        printf("%d eh multiplo de %d\n", numero1, numero2);
-    } else {
-        printf("%d nao eh multiplo de %d\n", numero2, numero1);
+        printf("%d eh maior que %d\n", numero2, numero1);
+    }
+
+    if (numero1 != numero2) {
+        if (numero1 % numero2 == 0 || numero2 % numero1 == 0) {
+            printf("%d eh multiplo de %d\n", numero1, numero2);
+        } else {
+            printf("%d nao eh multiplo de %d\n", numero2, numero1);
         }
-    } 
-    
+    }
+
     return 0;
 }
