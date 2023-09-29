35861e016b1fc4b2c69624d0a56e38b6b6ed7c3b
Quiquiles <129068122+Quiquiles@users.noreply.github.com>
2023-09-28 22:15:48 -0300

    Update inPAR.c

--- inPAR.c
+++ inPAR.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
-int main() {
-   
+int main()
+{
+
 	int calculo, soma, dado;
 	scanf("%d", &calculo);
-	
-	if(calculo % 2 == 0){
-	 printf("%d eh par\n", calculo);
-	} else{
-	 printf("%d eh impar\n", calculo);
+
+	if (calculo % 2 == 0)
+	{
+		printf("%d eh par\n", calculo);
 	}
-	
+	else
+	{
+		printf("%d eh impar\n", calculo);
+	}
+
 	soma = 0;
 	printf("A soma dos algarismos de %d", calculo);
-	while(calculo > 0){
-	     dado = calculo % 10;
-	     soma = soma + dado;
-	     calculo = calculo / 10;
+	while (calculo > 0)
+	{
+		dado = calculo % 10;
+		soma = soma + dado;
+		calculo = calculo / 10;
 	}
-	
+
 	printf(" eh igual a %d\n", soma);
 
-    return 0;
+	return 0;
 }
